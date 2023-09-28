0ed748835d26f9c07535477ee7135e8eff353bc7
reza adinepour <61472565+rezaAdinepour@users.noreply.github.com>
2023-09-28 14:39:39 +0330

    Add files via upload

---
+++ Labs/MicroProcessor/solution_01/Lab 3/Code/Part3/UP_Lab3_Part3.c
@@ -0,0 +1,118 @@
+//           ******************************************************
+//          **   Processor      : ATMEGA 32                       **
+//         ***   Frequency      : 8MHz External Clock             ***
+//        ****   AUTHOR         : Reza Adinepour                  ****
+//        ****   Linkedin       : linkedin.com/reza_adinepour/    ****
+//         ***   Student ID:    : 9814303                         ***
+//          **   Github         : github.com/reza_adinepour/      **
+//           ******************************************************
+
+
+#include <mega32.h>
+#include <delay.h>
+
+unsigned char help[] = {0x89, 0x86, 0xc7, 0x8c}, i;
+
+void main(void)
+{
+    DDRA = 0xff;
+    PORTA = 0x00;
+    
+    DDRB = 0xff;
+    PORTB = 0x00;
+    
+    DDRC = 0xff;
+    PORTC = 0x00;
+    
+    DDRD = 0xff;
+    PORTD = 0x00;
+
+
+    while (1)
+    {
+        PORTA = 0x01;
+        PORTB = help[0];
+        delay_ms(1000);
+        
+        for(i = 0; i < 100; i++)
+        {
+            PORTA = 0x01;
+            PORTB = help[0];
+            delay_ms(10);
+        
+            PORTA = 0x02;
+            PORTB = help[1];
+            delay_ms(10);   
+        }
+        
+        for(i = 0; i < 67; i++)
+        {
+            PORTA = 0x01;
+            PORTB = help[0];
+            delay_ms(10);
+        
+            PORTA = 0x02;
+            PORTB = help[1];
+            delay_ms(10);
+            
+            PORTA = 0x04;
+            PORTB = help[2];
+            delay_ms(10);   
+        }
+        
+        for(i = 0; i < 50; i++)
+        {
+            PORTA = 0x01;
+            PORTB = help[0];
+            delay_ms(10);
+        
+            PORTA = 0x02;
+            PORTB = help[1];
+            delay_ms(10);
+            
+            PORTA = 0x04;
+            PORTB = help[2];
+            delay_ms(10);
+            
+            PORTA = 0x08;
+            PORTB = help[3];
+            delay_ms(10);   
+        }
+        
+        for(i = 0; i < 67; i++)
+        {
+            PORTA = 0x02;
+            PORTB = help[1];
+            delay_ms(10);
+        
+            PORTA = 0x04;
+            PORTB = help[2];
+            delay_ms(10);
+            
+            PORTA = 0x08;
+            PORTB = help[3];
+            delay_ms(10);  
+        }
+        
+        for(i = 0; i < 100; i++)
+        {
+            PORTA = 0x04;
+            PORTB = help[2];
+            delay_ms(10);
+        
+            PORTA = 0x08;
+            PORTB = help[3];
+            delay_ms(10); 
+        }
+        PORTA = 0x08;
+        PORTB = help[3];
+        delay_ms(1000);   
+        
+        
+        
+        
+        
+        
+
+    }
+}
